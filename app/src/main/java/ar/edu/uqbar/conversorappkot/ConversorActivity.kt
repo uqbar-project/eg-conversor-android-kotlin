84d29d01b8d45bb869936e46861c0b646dd7803b
fdodino <fernando.dodino@gmail.com>
2017-11-09 10:32:06 -0300

    Enhanced icon + using Kotlin Android Extensions

--- app/src/main/java/ar/edu/uqbar/conversorappkot/ConversorActivity.kt
+++ app/src/main/java/ar/edu/uqbar/conversorappkot/ConversorActivity.kt
@@ -3,10 +3,9 @@ package ar.edu.uqbar.conversorappkot
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import java.text.DecimalFormat
+import kotlinx.android.synthetic.main.activity_conversor.*
 
 class Conversor {
     var millas = 0.0
@@ -27,21 +26,22 @@ class ConversorActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_conversor)
-        findViewById(R.id.conversor_convertir).setOnClickListener(this)
+        conversor_convertir.setOnClickListener(this)
     }
 
     override fun onClick(view: View?) {
         try {
-            val millas = (findViewById(R.id.conversor_millas) as EditText).text.toString()
+            val millas = conversor_millas.text.toString()
+            if (millas.trim().equals("")) {
+                Toast.makeText(this.getApplicationContext(), "Debe ingresar un valor", Toast.LENGTH_LONG).show()
+                return
+            }
             conversor.millas = java.lang.Double.valueOf(millas)
             conversor.convertir()
-            (findViewById(R.id.conversor_kilometros) as TextView).setText(conversor.kilometrosAsString())
-        } catch (e: NullPointerException) {
-            Toast.makeText(this.getApplicationContext(), "Debe ingresar un valor", Toast.LENGTH_LONG).show();
+            conversor_kilometros.text = conversor.kilometrosAsString()
         } catch (e: NumberFormatException) {
             Toast.makeText(this.getApplicationContext(), "Debe ingresar un valor numérico", Toast.LENGTH_LONG).show();
         }
     }
-
 
 }
